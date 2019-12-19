ff85ced369111522a87f910b5da0162febee216d
Mohammed Adenwala <adenwala@outlook.com>
2019-12-19 13:17:18 -0600

    Added book downloader

---
+++ Source/DownloadBooks.cmd
@@ -0,0 +1,2 @@
+dotnet build --source eSahifa.Books.Downloader.sln --configuration Release
+eSahifa.Books.Downloader\bin\Release\eSahifa.Books.Downloader.exe ..\Books
---
+++ Source/eSahifa.Books.Downloader/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace eSahifa.Books.Downloader
+{
+    class Program
+    {
+        private string[] _booksList = {
+            "https://esahifa.com/books/bk00001",
+        };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var directory = args.FirstOrDefault() ?? Environment.CurrentDirectory;
+                Console.WriteLine("Output Directory: " + directory);
+                int bookNumber = 1;
+                while (true)
+                {
+                    bool completed = DownloadBook(directory, bookNumber).Result;
+                    if (completed == false)
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("APPLICATION FAILURE!");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static async Task<bool> DownloadBook(string directory, int bookNumber)
+        {
+            var di = Directory.CreateDirectory(Path.Combine(directory, "Book" + bookNumber));
+            Console.WriteLine("Output Directory: " + di.FullName);
+            Console.WriteLine($"Starting download of Book {bookNumber}...");
+
+            int pageNumber = 1;
+            while (true)
+            {
+                Console.Write($"Downloading book {bookNumber} page {pageNumber}...");
+
+                try
+                {
+                    string fileName = $"{pageNumber:D3}.jpg";
+                    string filePath = di.FullName + "\\" + fileName;
+
+                    if (!File.Exists(filePath))
+                    {
+                        using (HttpClient client = new HttpClient())
+                        {
+
+                            //send  request asynchronously
+                            using (HttpResponseMessage response = await client.GetAsync($"https://esahifa.com/books/bk00001/{fileName}"))
+                            {
+                                // Check that response was successful or throw exception
+                                response.EnsureSuccessStatusCode();
+
+                                // Read response asynchronously and save asynchronously to file
+                                using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                                {
+                                    //copy the content from response to filestream
+                                    await response.Content.CopyToAsync(fileStream);
+                                }
+                            }
+                        }
+                        Console.WriteLine("DONE!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("already exists.");
+                    }
+
+                    pageNumber++;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("FAILED!");
+                    break;
+                }
+            }
+
+            if (di.Exists && di.GetFiles().Length == 0)
+            {
+                Console.WriteLine($"Book {bookNumber} does not exist.");
+                di.Delete(true);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine($"Book {bookNumber} has been downloaded!");
+                return true;
+            }
+        }
+    }
+}
+
+
